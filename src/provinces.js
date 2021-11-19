19db8bf88193e9908752c00a9cbbb68134dcd529
Spencer-Lukens <86165771+Spencer-Lukens@users.noreply.github.com>
2021-11-19 07:46:45 -0800

    first commit

---
+++ src/provinces.js
@@ -0,0 +1,17 @@
+import 'react-number-format'
+
+const abData = [
+    population: 5100
+] 
+const bcData = [5,  ]
+const mbData = []
+const nbData = []
+const nlData = []
+const nsData = []
+const ntData = []
+const nuData = []
+const onData = []
+const pieData = []
+const qcData = []
+const skData = []
+const ytData = []
