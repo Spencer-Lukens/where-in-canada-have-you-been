13f54a08e355883712744048ab3cf93a95abf650
Spencer-Lukens <86165771+Spencer-Lukens@users.noreply.github.com>
2021-11-25 06:37:37 -0800

    updated colors, played around with buttons and scaling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { useEffect, useState } from 'react';
 import firebase from './firebase.js';
 
 function App() {
-    // We want a new component thats storing our state item, I.E our books. Consider this for country/setCountry
-  // inside of UL we want to append our books array with map
-  // For every book, we want to return a certain type of mark up
+    // We want a new component thats storing our state item, I.E our province/setProvinces
+  // inside of UL we want to append our province array with map
+  // For every province, we want to return a certain type of mark up
   // {book} will be a component containing the properties (i.E title) hosted in our firebase database
   const [provinces, setProvinces] = useState([])
   // this hooks into any CHANGE that takes place on the input field
@@ -28,27 +28,23 @@ function App() {
     // push that data
     dbRef.push(userInput);
 
-    // a nice benefit of this state, of pushing our stored input field data, is that we can say once the user submits, let's clear out the input field (useful for multiple visitors!)
-
-    // Once we call to firebase, let's call that setUserInput method and set our state back to an empty string
+    // When user submits, clear out input field
+    // Once call to firebase is completed, calling the setUserInput method and resetting state to an empty string
     setUserInput('');
 
   }
 
   useEffect(() => {
-    // Make a reference to our database
+    // Making a reference to our database
     const dbRef = firebase.database().ref();
-    // code that goes into LISTENING for the CHANGING OF VALUE (I.E CLICK STATE that changes firebase data). Basically setting up a listening event within firebase database
+    // listening for changing of value within firebase
     dbRef.on('value', (response) => {
       // console.log(response.val());
       const newState = [];
-
       const data = response.val()
-
       // iterate through the data object
       for (let property in data) {
         // so basically, push each book name into the new Array
-
         // alternative - newState.push(data[property]) then 'book title', 'another book title'
         newState.push({
           provinceTitle: data[property],
@@ -85,7 +81,7 @@ function App() {
             <div className="listContainer">
               <h3>Your itinerary</h3>
               <div className="ulContainer">
-                <ul>
+                <ol>
                   {
                     provinces.map((province) => {
                       return (
@@ -101,7 +97,7 @@ function App() {
                       )
                     })
                   }  
-                </ul>
+                </ol>
 
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="newProvince">Add a new province to your itinerary!</label>
@@ -119,8 +115,6 @@ function App() {
             <div className="primaryContainer">
               <div className="inputArea">
                 <h2 className="provinces">The 13 Provinces of Canada</h2>
-                  <h3>Search the provinces you'd like to visit!
-                  </h3>
                   {/* <button>Add province to list</button>
                   <button>Remove province from list</button> */}
               </div>
